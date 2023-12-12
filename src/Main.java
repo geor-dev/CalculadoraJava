5234c6e8591a0e9d04116f1619a96d6bb3c13e87
Adryan <adryangeorgem@gmail.com>
2023-12-12 09:30:41 -0400

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String [] args){
+        int num1 = 20;
+        int num2 = 20;
+        System.out.println("A soma e: " + (num1+num2));
+        System.out.println("A subtracao idade: " + (num1 - num2));
+        System.out.println("A multiplicacao e: " + (num1*num2));
+        System.out.println("A divisao e: " + (num1/num2));
+        System.out.println("O modulo e: " + (num1 % num2));
+    }
+}
